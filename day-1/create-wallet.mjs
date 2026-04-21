4dfd867a5cbd4fde00c18f414eae5c2589993366
Akeem Palmer <akeemk.palmer@gmail.com>
2026-04-20 21:03:09 -0400

    Completed: day 1

--- day-1/create-wallet.mjs
+++ day-1/create-wallet.mjs
@@ -1,4 +1,4 @@
-import {generateKeyPairSigner, createSolanaRpc, devnet} from "@solana/kit"
+import {generateKeyPairSigner, createSolanaRpc, devnet, address} from "@solana/kit"
 
 const rpc = createSolanaRpc(devnet("https://api.devnet.solana.com"));
 
@@ -6,11 +6,15 @@ const rpc = createSolanaRpc(devnet("https://api.devnet.solana.com"));
 const wallet = await generateKeyPairSigner();
 
 console.log("Your new wallet address: ", wallet.address);
+console.log("\n- Go to https://faucet.solana.com/ and airdrop SOL to this address -");
 console.log(
-    "\nThis address is your public key. It's safe to share."
+    "- Then run this script again with the same address to check the balance -\n"
 );
-console.log(
-    "The private key stays in memory. In a real app, you'd save it securely."
-)
 
-// FwoeAGnyP4qrbR44ZYWWrFcpjkuhRe4YZDCXBNHRrwNY
+// To check a specific address you've already funded, replace the line below:
+const {value: balance } = await rpc.getBalance(address("2DMz9JZowNTJg6NknHqF8RKcUpXUmxhg9NmQqztmW1vF")).send();
+
+// const { value: balance } = await rpc.getBalance(wallet.address).send();
+const balanceInSol = Number(balance) / 1_000_000_000;
+
+console.log(`Balance: ${balanceInSol} SOL`);
